58d13fc1318eef9476d03da48ecf530385a16527
motsoane012 <ranchobemotsoane48@gmail.com>
2026-04-09 13:33:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ from datetime import datetime
 import pymysql
 
 app = Flask(__name__)
-CORS(app, origins=[
-    "https://cs-3430.vercel.app",
-    "http://127.0.0.1:5500",
-    "http://localhost:5500"
-])
+CORS(app)
 
 # ==========================
 # DATABASE CONFIG
